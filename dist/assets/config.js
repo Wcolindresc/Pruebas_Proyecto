d42349c19c2d7c4eb6762a9aadd3d2467e6f1f6f
Wcolindresc <wcolindresc@miumg.edu.gt>
2025-10-22 09:33:36 -0600

    Update config.js

--- dist/assets/config.js
+++ dist/assets/config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
-  API_BASE_URL: 'https://your-backend.example.com', // <-- CAMBIA a tu URL del backend Flask
-  SUPABASE_URL: '',                                  // <-- Pega tu URL de Supabase
-  SUPABASE_ANON_KEY: ''                              // <-- Pega tu anon key
+  API_BASE_URL: 'https://pruebas-proyecto-5bna.onrender.com', // <-- CAMBIA a tu URL del backend Flask
+  SUPABASE_URL: 'https://stihjrzjqjsnkxzjgsvr.supabase.co',                                  // <-- Pega tu URL de Supabase
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0aWhqcnpqcWpzbmt4empnc3ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNjkxOTMsImV4cCI6MjA3NjY0NTE5M30.-QhijTwU2JO6TW7l1YnwvJV9ZeMlih01GPww9SjfmIs'                              // <-- Pega tu anon key
 };
 
