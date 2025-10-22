e125cdd8cd68aa60a4b59309ec6e314609d04ea1
Wcolindresc <wcolindresc@miumg.edu.gt>
2025-10-22 12:43:56 -0600

    Update config.js

--- dist/assets/config.js
+++ dist/assets/config.js
@@ -1,6 +1,6 @@
 window.__CONFIG__ = {
-  API_BASE_URL: 'https://pruebas-proyecto-5bna.onrender.com', // <-- CAMBIA a tu URL del backend Flask
-  SUPABASE_URL: 'https://stihjrzjqjsnkxzjgsvr.supabase.co',                                  // <-- Pega tu URL de Supabase
-  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InN0aWhqcnpqcWpzbmt4empnc3ZyIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwNjkxOTMsImV4cCI6MjA3NjY0NTE5M30.-QhijTwU2JO6TW7l1YnwvJV9ZeMlih01GPww9SjfmIs'                              // <-- Pega tu anon key
+  API_BASE_URL: 'https://pruebas-proyecto-fsxd.onrender.com', // <-- CAMBIA a tu URL del backend Flask
+  SUPABASE_URL: 'https://https://tawahrfwqohumoyeeugi.supabase.co',                                  // <-- Pega tu URL de Supabase
+  SUPABASE_ANON_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InRhd2FocmZ3cW9odW1veWVldWdpIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjEwODQ3MjgsImV4cCI6MjA3NjY2MDcyOH0.ZUWL1IMtGRIkdWfEaJGS1rBNtL7v67RfqluRgfDKMKM'                              // <-- Pega tu anon key
 };
 
